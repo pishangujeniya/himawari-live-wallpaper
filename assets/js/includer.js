c188197f6f4373d497cafe7eb04eb2474e749a2c
Pishang Ujeniya <pishangujeniya@outlook.com>
2019-04-06 05:34:39 +0530

    production release removed boostrap cdn and added npm module

--- assets/js/includer.js
+++ assets/js/includer.js
@@ -1,12 +1,11 @@
+var $ = require('jquery');
 var fs = require('fs-extra');
 var path = require('path');
 var { ipcRenderer } = require("electron");
 var wallpaper = require('wallpaper');
 var rootPath = require('electron-root-path').rootPath;
 var path = require('path');
-var himawari = require(path.join(rootPath, "/assets/js/himawari_scrapper.js"));
 var dateFormat = require('dateformat');
-var $ = require('jquery');
 var async = require('async');
 var crypto = require('crypto');
 var extend = require('deep-extend');
--- main.js
+++ main.js
@@ -1,80 +1,81 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
-const {download} = require("electron-dl");
+const { download } = require("electron-dl");
 var path = require('path')
 var url = require('url')
 var iconpath = path.join(__dirname, './assets/images/icons8-globe-100.png') // path of y
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
+
 let mainWindow
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 950,
-    icon: iconpath,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 950,
+        icon: iconpath,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  mainWindow.setMenu(null);
+    // mainWindow.setMenu(null);
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
 
-  var appIcon = new Tray(iconpath)
+    var appIcon = new Tray(iconpath)
 
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Show App', click: function () {
-        mainWindow.show()
-      }
-    },
-    {
-      label: 'Quit', click: function () {
-        app.isQuiting = true
-        app.quit()
-      }
-    }
-  ])
+    var contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show App', click: function () {
+                mainWindow.show()
+            }
+        },
+        {
+            label: 'Quit', click: function () {
+                app.isQuiting = true
+                app.quit()
+            }
+        }
+    ])
 
-  appIcon.setContextMenu(contextMenu)
-  appIcon.setTitle("Live Wallpaper")
-  appIcon.setToolTip("Live Wallpaper");
-
-
-  mainWindow.on('close', function (event) {
-    mainWindow = null
-  })
-
-  mainWindow.on('minimize', function (event) {
-    event.preventDefault()
-    mainWindow.hide()
-  })
-
-  mainWindow.on('show', function () {
-    appIcon.setHighlightMode('always')
-  })
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    appIcon.setContextMenu(contextMenu)
+    appIcon.setTitle("Live Wallpaper")
+    appIcon.setToolTip("Live Wallpaper");
 
 
-  ipcMain.on("download", (event, info) => {
-    info.properties.onProgress = status => mainWindow.webContents.send("download_progress", status);
-    download(mainWindow, info.url, info.properties)
-      .then(dl => mainWindow.webContents.send("download_complete", dl.getSavePath()));
-  })
+    mainWindow.on('close', function (event) {
+        mainWindow = null
+    })
+
+    mainWindow.on('minimize', function (event) {
+        event.preventDefault()
+        mainWindow.hide()
+    })
+
+    mainWindow.on('show', function () {
+        appIcon.setHighlightMode('always')
+    })
+
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+
+    ipcMain.on("download", (event, info) => {
+        info.properties.onProgress = status => mainWindow.webContents.send("download_progress", status);
+        download(mainWindow, info.url, info.properties)
+            .then(dl => mainWindow.webContents.send("download_complete", dl.getSavePath()));
+    })
 
 
 }
@@ -86,19 +87,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
